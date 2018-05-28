db930836909d90d9155b5268d5483e38cc5e26aa
Kannnnng <575664647@qq.com>
2018-05-28 20:29:37 +0800

    小改动

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import 'mint-ui/lib/style.css'
 
 //axios配置
 axios.defaults.baseURL = '/api/v1';
-axios.defaults.headers.common['Authorization'] = 'QJWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsInVhIjoicGMiLCJjb21wbGV0ZSI6dHJ1ZSwiaWF0IjoxNTI2NTIxODUxLCJleHAiOjE1MjY2MDgyNTF9.jRrHrtMVUDd0ou5gBRJNZIBBd0GZOxW2JUzgjniMJnU';
+axios.defaults.headers.common['Authorization'] = 'QJWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidWEiOiJwYyIsImNvbXBsZXRlIjp0cnVlLCJzdGF0ZSI6Ii9wbGF5ZXIvNWIwMzczZjI4NTc1NGEyN2FlMGMyZmY1IiwiaWF0IjoxNTI3NTA5Njg2LCJleHAiOjE1Mjc1OTYwODZ9.32srbACbpxN-B0RxaeFJhJRBUPc2q7N-a5UinuLMXQc';
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
